01b9177ecf121068df6de2ea1485fff7240e926b
ashrouge <raflyputra0510@gmail.com>
2023-05-10 05:35:41 +0700

    fix burger

--- components/adminpage/utils/navbar.js
+++ components/adminpage/utils/navbar.js
@@ -1,4 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
+
+  const change = (e) => {
+    e.preventDefault()
+    if (document.body.classList.contains("sidebar-mini") && screen.width > 1024){
+      document.body.classList.remove("sidebar-mini")
+    } else if (!document.body.classList.contains("sidebar-mini") && screen.width > 1024) {
+      document.body.classList.add("sidebar-mini")
+    } else if (document.body.classList.contains("sidebar-gone") && screen.width <= 1024) {
+      document.body.classList.add("sidebar-show")
+      document.body.classList.remove("sidebar-gone")
+    } else if (document.body.classList.contains("sidebar-show") && screen.width <= 1024) {
+      document.body.classList.remove("sidebar-show")
+      document.body.classList.add("sidebar-gone")
+    }
+  }
+
 
 export default function Navbar() {
   return (
@@ -8,7 +24,7 @@ export default function Navbar() {
         <form className="form-inline mr-auto">
           <ul className="navbar-nav mr-3">
             <li>
-              <a data-toggle="sidebar"  className="nav-link nav-link-lg">
+              <a data-toggle="sidebar" onClick={change}  className="nav-link nav-link-lg">
                 <i className="fas fa-bars" />
               </a>
             </li>
---
+++ components/adminpage/utils/scripts.js
@@ -0,0 +1,27 @@
+export default function Scripts() {
+    return (
+      <>
+        <script src="adminpage/js/jquery.min.js" />
+        <script src="adminpage/js/popper.js" />
+        <script src="adminpage/js/tooltip.js" />
+        <script src="adminpage/js/bootstrap.min.js" />
+        <script src="adminpage/js/jquery.nicescroll.min.js" />
+        <script src="adminpage/js/moment.min.js" />
+        <script src="adminpage/js/stisla.js" />
+        <script src="adminpage/js/summernote-bs4.js" />
+        <script src="adminpage/js/jquery.chocolat.min.js" />
+  
+        <script src="adminpage/js/index-0.js" />
+  
+        <script src="adminpage/js/scripts.js" />
+        <script src="adminpage/js/custom.js" />
+        <script src="adminpage/js/jquery-ui.min.js" />
+  
+        <script src="dist/js/dataTables.min.js" />
+        <script src="dist/js/dataTablesbs.min.js" />
+        <script src="dist/js/dataTablesbsselect.min.js" />
+        <script src="dist/js/modul-datatables.js" />
+        
+      </>
+    )
+  }
