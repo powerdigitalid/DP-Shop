4b64db5d511cf6e8e902d65b406309cbb1aec0cb
AlvifSandana <asm240800@gmail.com>
2023-05-28 22:23:40 +0700

    update schema and seeder

--- pages/api/order/cart.js
+++ pages/api/order/cart.js
@@ -3,24 +3,23 @@
 import { prisma } from "../../../libs/prisma.libs";
 
 //create hangle product to cart 
-export default async (req, res) => {
-    if (req.method === "POST") {
-        const { user_google, product_id, product_price,product_name,quantity, total} = req.body;
-        const cart = await prisma.cart.create({
-            data: {
-                user_google : user_google,
-                product: {
-                    connect: {
-                        id: parseInt(product_id),
-                    },
-                },
-                product_name: product_name, //how to fetch data product_name
-                product_price: parseInt(product_price), //how to fetch data product_price
-                quantity: parseInt(quantity),
-                total: parseInt(total),
-            },
-        });
-        return res.status(200).json(cart);
-    }
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { user_google, product_id, product_price, product_name, quantity, total } = req.body;
+    const cart = await prisma.cart.create({
+      data: {
+        user_google: user_google,
+        product: {
+          connect: {
+            id: parseInt(product_id),
+          },
+        },
+        product_name: product_name, //how to fetch data product_name
+        product_price: parseInt(product_price), //how to fetch data product_price
+        quantity: parseInt(quantity),
+        total: parseInt(total),
+      },
+    });
+    return res.status(200).json(cart);
+  }
 }
-    
